090403afc771c3a74df797ed95e364691d5bb990
tristinxie <tristinxie@gmail.com>
2026-03-15 16:56:07 -0700

    Implement more grid functions, add controls

--- src/grid.ts
+++ src/grid.ts
@@ -1,13 +1,14 @@
 import { Cell } from "./cell.ts";
 type Grid = {
-	rows: number,
-	columns: number,
-	grid: Cell[][],
-	clearGrid(): void,
-	setCellAlive(x: number, y: number): void,
-	setCellDead(x: number, y: number): void,
-	setCellsAlive(x: number[], y: number[]): void,
-	setCellsDead(x: number[], y: number[]): void,
+	rows: number;
+	columns: number;
+	grid: Cell[][];
+	clearGrid(): void;
+	setCellAlive(x: number, y: number): void;
+	setCellDead(x: number, y: number): void;
+	toggleCell(x: number, y: number): void;
+	setCellsAlive(x: number[], y: number[]): void;
+	setCellsDead(x: number[], y: number[]): void;
 }
 
 class ConwayGrid implements Grid {
@@ -21,13 +22,16 @@ class ConwayGrid implements Grid {
 		this.columns = columns;
 	}
 	clearGrid(): void {
-		throw new Error("Method not implemented.");
+		this.grid = Array.from({length: this.rows}, () => Array.from({length: this.columns}, () => new Cell(false)));
 	}
 	setCellAlive(x: number, y: number): void {
 		this.grid[y][x].alive = true;
 	}
 	setCellDead(x: number, y: number): void {
-		throw new Error("Method not implemented.");
+		this.grid[y][x].alive = false;
+	}
+	toggleCell(x: number, y: number): void {
+		this.grid[y][x].alive ? this.setCellDead(x, y) : this.setCellAlive(x, y);
 	}
 	setCellsAlive(x: number[], y: number[]): void {
 		throw new Error("Method not implemented.");
--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,15 @@ const init = (): void => {
   const template = GridTemplate.instance;
   template.canvas.addEventListener("click", (event) => {
     const [x, y] = template.clickCoords(event);
-    conwayGrid.setCellAlive(x, y);
+    conwayGrid.toggleCell(x, y);
     console.log(`x: ${x}, y: ${y}`);
     console.log(conwayGrid.grid);
     template.render(conwayGrid);
+  })
+  const clearGrid = document.getElementById("clear");
+  clearGrid?.addEventListener("click", (): void => {
+    conwayGrid.clearGrid();
+    template.clear();
   })
   template.render(conwayGrid);
 }
